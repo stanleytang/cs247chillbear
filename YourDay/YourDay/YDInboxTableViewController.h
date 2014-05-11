de8d1563d7d8ce09e84fd7b8defe3782f2a56ed8
Andy Mai <andymai@rescomp-13-213410.stanford.edu>
2014-05-11 16:27:50 -0700

    added storyboard stuff

---
+++ YourDay/YourDay/YDInboxTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  YDInboxTableViewController.h
+//  YourDay
+//
+//  Created by Andy Mai on 5/11/14.
+//  Copyright (c) 2014 ChillBear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YDInboxTableViewController : UITableViewController
+
+@end
